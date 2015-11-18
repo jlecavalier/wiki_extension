4143585aa8ad3954db69860802f643a0d276f89b
Jay LeCavalier <jay.lecavalier@colorado.edu>
2015-11-17 20:10:32 -0700

    Pull content from a Wikipedia page

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,16 +2,34 @@ window.onload = function () {
   function doSearch() {
     var title = document.getElementById('search_input').value;
     MediaWikiJS('https://en.wikipedia.org', 
-      {action: 'query', prop: 'revisions', titles: title}, function (data) {
-    var pages = data.query.pages;
-    if (pages[Object.keys(pages)[0]].revisions != undefined) {
-      document.getElementById('wiki_info').innerHTML = 
-        ('Last edited by: ' + pages[Object.keys(pages)[0]].revisions[0].user);
-    }
-    else {
-      document.getElementById('wiki_info').innerHTML = "No results found :(";
-    }
+      {action: 'query', prop: 'info', inprop: 'url', titles: title}, function (data) {
+      var pages = data.query.pages;
+      console.log(pages);
+      if (pages[Object.keys(pages)[0]].missing == undefined &&
+          pages[Object.keys(pages)[0]].invalid == undefined) {
+        
+        document.getElementById('page_link').href = 
+          pages[Object.keys(pages)[0]].canonicalurl;
+
+        MediaWikiJS('https://en.wikipedia.org',
+          {action: 'parse', prop: 'text|displaytitle', page: title}, function (data) {
+          var textdata = data.parse.text['*'];
+          var titledata = data.parse.displaytitle;
+          console.log(textdata);
+          console.log(titledata);
+          document.getElementById('page_link').innerHTML = titledata;
+          document.getElementById('test').innerHTML = textdata;
+          })
+      }
+      else {
+        document.getElementById('page_link').innerHTML = "No results found :(";
+      }
     });
   }
+  document.getElementById('search_input').onkeypress = (function(e){
+    if (e.keyCode == 13) {
+      document.getElementById('search_submit').click();
+    }
+  });
   document.getElementById('search_submit').onclick = doSearch;
 };
